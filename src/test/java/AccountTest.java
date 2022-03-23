4dea0e9d4ebdfd35fd3905a57bda2dc2a7ce3285
TatianaKydryashova <kudryashova_ts@mail.ru>
2022-03-23 13:05:51 +0300

    add project

---
+++ src/test/java/AccountTest.java
@@ -0,0 +1,38 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+@RunWith(Parameterized.class)
+public class AccountTest {
+    private final String name;
+    private final boolean expectedResult;
+
+    public AccountTest(String name, boolean expectedResult) {
+        this.name = name;
+        this.expectedResult = expectedResult;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getNameData() {
+        return new Object[][] {
+                { "Тимоти Шаламе", true},
+                { "Тимоти Шаламе Шаламе", false},
+                { "ТимотейШевроле", false},
+                { "123Тимотей", false},
+                { "Timoty 789", false},
+                { "2,7894 789654", false},
+                { " Timoty Shalame ", false},
+                { "Тимоти Тимоти ТимотиТимотиТимоти-Шаламе ", false},
+                { "Ти", false},
+                { "", false},
+                {null, false},
+        };
+    }
+
+    @Test
+    public void checkNameToEmboss() {
+        Account account = new Account(name);
+        assertEquals(expectedResult, account.checkNameToEmboss());
+    }
+}
